fdea19444dbfd3003d190585707b4a019f07bb24
Wade Novak <wade.novak@gmail.com>
2015-02-21 00:08:05 -0600

    Adds working Google Map API with geolocating

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,54 @@
+// function initialize() {
+//   var mapOptions = {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 10,
+//   };
+//   var map = new google.maps.Map(document.getElementById('map'),
+//       mapOptions);
+
+//   var marker = new google.maps.Marker({
+//     position: { lat: -34.555, lng: 150.777 },
+//     map: map,
+//     title:"Hello World!"
+//   });
+
+//   var marker2 = new google.maps.Marker({
+//     position: { lat: -34.888, lng: 150.555 },
+//     map: map,
+//     title:"Hello World!"
+//   });
+// }
+
+var plotMarkers = function() {
+  if (window.Locations == undefined) {
+    return;
+  }
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var currentPosition = new google.maps.LatLng(
+        position.coords.latitude,
+        position.coords.longitude
+      );
+
+      var map = new google.maps.Map(document.getElementById('map'), {
+        center: currentPosition,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      });
+
+      for (var i = 0; i < window.Locations.length; i++) {
+        var place = window.Locations[i];
+        var position = new google.maps.LatLng(place.latitude, place.longitude);
+
+        var marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            draggable: false
+        });
+      };
+    });
+  }
+};
+
+$(document).ready(plotMarkers)
