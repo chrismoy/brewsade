8ed542d5c5392751bf73511e0c0c833b658a6ed0
Chris Moy <moyboy530@gmail.com>
2015-02-23 15:24:08 -0600

    Make google map dynamically update markers (AJAX)

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,4 +1,44 @@
-var plotMarkers = function() {
+// var plotMarkers = function() {
+//   if (window.Matches == undefined) {
+//     return;
+//   }
+
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(function (position) {
+//       var currentPosition = new google.maps.LatLng(
+//         position.coords.latitude,
+//         position.coords.longitude
+//       );
+
+//       var map_id = document.getElementById('map');
+//       var map_options = {
+//         center: currentPosition,
+//         zoom: 15,
+//         mapTypeId: google.maps.MapTypeId.ROADMAP
+//       }
+
+//       var map = new google.maps.Map(map_id, map_options);
+
+//       for (var i = 0; i < window.Matches.length; i++) {
+//         var place = window.Matches[i];
+//         var position = new google.maps.LatLng(place.latitude, place.longitude);
+
+//         var marker = new google.maps.Marker({
+//             position: position,
+//             map: map,
+//             draggable: false
+//         });
+//       };
+//     });
+//   }
+// };
+
+// $(document).ready(plotMarkers)
+
+var markerCount = 0;
+var formSubmit = $("#tip-submit");
+
+function initialize() {
   if (window.Matches == undefined) {
     return;
   }
@@ -10,24 +50,60 @@ var plotMarkers = function() {
         position.coords.longitude
       );
 
-      var map = new google.maps.Map(document.getElementById('map'), {
+      var map_id = document.getElementById('map');
+      var map_options = {
         center: currentPosition,
         zoom: 15,
         mapTypeId: google.maps.MapTypeId.ROADMAP
-      });
+      }
+
+      map = new google.maps.Map(map_id, map_options);
 
       for (var i = 0; i < window.Matches.length; i++) {
         var place = window.Matches[i];
         var position = new google.maps.LatLng(place.latitude, place.longitude);
 
-        var marker = new google.maps.Marker({
-            position: position,
-            map: map,
-            draggable: false
-        });
+        addMarker(position, map);
       };
     });
   }
 };
 
-$(document).ready(plotMarkers)
+function addMarker(position, map) {
+  var marker = new google.maps.Marker({
+      position: position,
+      map: map,
+      draggable: false
+  });
+  console.log("hello");
+};
+
+$(document).ready(initialize);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
